75ec9acf629bccef01b213f034234c916935e8fb
en3873 <en3873@gmail.com>
2023-01-29 04:10:47 +0900

    PyQgis_0128

--- fn_ClusterByDen.py
+++ fn_ClusterByDen.py
@@ -2,59 +2,64 @@
 
 import os
 import processing
-from qgis.core import QgsProject
+from qgis.core import QgsProject, QgsVectorLayer
 from qgis.analysis import QgsNativeAlgorithms
 
 def KmeansByDen(input):
-
-    input_path = "/Users/sohyunkim/Desktop/works/부동산데이터작업/지역별건물shp/"+ input + "/" + input + "_CLIP"
-    output_path = "/Users/sohyunkim/Desktop/works/부동산데이터작업/지역별건물shp/"+ input + "/" + input + "_Kmeans"
+    
+    input_path = '/Users/sohyunkim/Desktop/works/부동산데이터작업/지역별건물shp/' + input + '/' + input + '_CLIP'
+    output_path = '/Users/sohyunkim/Desktop/works/부동산데이터작업/지역별건물shp/' + input + '/' + input + '_Kmeans'
     if not os.path.exists(output_path):
         os.makedirs(output_path)
+        print(output_path)
     
     i = 0
     for file in os.listdir(input_path):
-        layer = iface.addVectorLayer(input_path, "", "ogr")
-        
-        num_features = layer.featureCount()
-    
-        # 밀도 계산 (클러스터 개수 k를 결정하는 기준)
-        extent = layer.extent()
-        area = extent.width() * extent.height()
-        
-        # 0으로 나뉘면 안됨
-        if area == 0 :
-            density = 0
-            print("CLIP_", i, " - No Density : {}".format(density),"---", num_features, " features")
-        else :
-            density = num_features / area
-            print("CLIP_", i, " - Density: {}".format(density)," ", num_features, " features")
+        # input_path_ = input_path + '/INCHEON_CLIP_' + str(i) + '.shp'
+        # output_path_ = output_path + '/Cluster_' + str(i) + '.shp'
+        if file.endswith('.shp'):
+            file_path = os.path.join(input_path, file)
+            input_layer = QgsVectorLayer(file_path, 'input_layer', 'ogr')
+            if not input_layer.isValid():
+                print("Layer is not Available")
+            else:
+                num_features = input_layer.featureCount()
             
-        # density 기반 클러스터 개수 설정
-        if density == 0 or density >= 0.01 :
-            k = 1
-        elif density >= 0.005 :
-            k = 2
-        else :
-            k = 3
-        
-        ## k-means clustering 수행
-        result = processing.run("native:kmeansclustering", {
-            'INPUT' : layer,
-            'FIELD_NAME' : 'cluster_id',
-            'CLUSTERS' : k,
-            'OUTPUT' : output_path
-        })
-        # 현재 레이어 전체 삭제
-        project = QgsProject.instance()
-        layer_ids = project.mapLayers().keys()
+                # 밀도 계산 (클러스터 개수 k를 결정하는 기준)
+                extent = input_layer.extent()
+                area = extent.width() * extent.height()
+                
+                # 0으로 나뉘면 안됨
+                if area == 0 :
+                    density = 0
+                    print("CLIP_", i, " - No Density : {}".format(density),"---", num_features, " features")
+                else :
+                    density = num_features / area
+                    print("CLIP_", i, " - Density: {}".format(density)," ", num_features, " features")
+                    
+                # density 기반 클러스터 개수 설정
+                if density == 0 or density >= 0.01 :
+                    k = 1
+                elif density >= 0.005 :
+                    k = 2
+                else :
+                    k = 3
+                print(k)
 
-        for layer_id in layer_ids:
-            project.removeMapLayer(layer_id)
+                file_name = 'Kmeans_' + str(i) + '.shp'
+                output_path_ = os.path.join(output_path, file_name)
+                print(output_path_)
+                ## k-means clustering 수행
+                result = processing.run("native:kmeansclustering", {
+                    'INPUT' : input_layer,
+                    'FIELD_NAME' : 'cluster_id',
+                    'CLUSTERS' : k,
+                    'OUTPUT' : output_path_
+                })
+                output_layer = iface.addVectorLayer(output_path_, "", "ogr")
+                i += 1
 
-        i+=1
-        # Kmeans 적용된 output파일 경로출력
-        output_path_ = output_path + '/Cluster_' + str(i) + '.shp'
+                print("처리 완료 : " + output_path_)
 
 input = "INCHEON"
 KmeansByDen(input)
